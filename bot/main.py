2d103e4797106054d06a6e0fda118c784175231f
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-10-16 18:25:02 -0300

    Testando a biblioteca
    mudou quase nada

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,15 @@
 #  imports a serem usados
+import re
 import nltk
 
-class NTKL:
-    nltk.download('all')
-
-user_name = input('Digite como você deve ser chamado(a): ')
-
-while True:
-    user_chat = input()
-    if user_chat == 'sair':
-        break
+class bot:
+    texto = 'Um exemplo bem simples, mas que vai me ajudar eventualmente'
+    par = nltk.word_tokenize(texto)
+    print(par)
+    
+class usuario:
+    user_name = input('Digite como você deve ser chamado(a): ')
+    while True:
+        user_chat = input()
+        if user_chat == 'sair':
+            break
