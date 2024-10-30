65ce87530fae8ee1a9e56e6b4e7c5a455ccb7392
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-10-30 16:37:46 -0300

    add

--- bot/dados.py
+++ bot/dados.py
@@ -1,12 +1,14 @@
-#  imports a serem usados
+# imports
 import nltk
 from nltk.corpus import movie_reviews
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-    
+
 class Bot:
-    userChat = input()
+    def dados():
+        nltk.data.path.append(r"C:\Users\alves\OneDrive\Documentos\datas") # caminho pros .csv
     
-    def processamentos_mensagens(userChat):
-        print()
+    def mensagens_possiveis():
+        print
+        
