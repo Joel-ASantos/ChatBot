24e37598a6da8557adac8062f2bca0246f49fd10
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-11-11 19:16:46 -0300

    add datas and remove some lines

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,5 @@
 #  imports a serem usados
 import nltk
-from nltk.corpus import movie_reviews
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from dados import mensagens_possiveis
+from bot.bot import mensagens_possiveis
 
-userChat = input("envie uma mensagem: ")
-envioMensagens = mensagens_possiveis(userChat)
-print(envioMensagens)
+userChat = input("Digite: ")
