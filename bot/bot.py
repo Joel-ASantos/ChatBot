aebd0f433754c4ccfe31cd7b575a745323f9de07
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-11-12 17:28:36 -0300

    add and reform some lines

--- bot/bot.py
+++ bot/bot.py
@@ -1,26 +1,27 @@
 # imports
+import os
 import nltk
-from nltk.corpus import movie_reviews
+import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
-nltk.data.path.append(r"C:\Users\alves\OneDrive\Documentos\datas") # caminho pros .csv    
-# configurações iniciais
+# Configurações Iniciais
+dados_pasta = "dados"
+arquivosCSV = [arquivo for arquivo in os.listdir(dados_pasta) if arquivo.endswith('.csv')]
 stopwords = set(stopwords.words("Portuguese"))
 stemmer = PorterStemmer()    
-# respostas de inicio e fim
-respostas = {
-    "inicio":["Olá tudo bem?","Eae meu nobre, como vai?"],
-    "fim":["Tchau!","Flw!"]
-}
-def mensagens_possiveis(mensagem):
-   tokens = word_tokenize(mensagem.lower())
-   tokens = [stemmer.stem(word) for word in tokens if word not in stopwords]
-   
-   if any(word in tokens for word in ["eae", "oi", "olá"]): 
-       return respostas["inicio"] 
-   elif any(word in tokens for word in ["flw", "tchau"]):
-       return respostas["fim"] 
-   else: 
-       return ["Resposta indefinida"]
+
+# Leitura de Dados
+df = pd.DataFrame()
+for arquivo in arquivosCSV:
+    caminho = os.path.join(dados_pasta,arquivo)
+    df = df.append(pd.read_csv(caminho), ignore_index=True)
+
+# Reposta
+dicio = {}
+
+# Função para as mensagens
+def mensagens(mensagem):
+    tokens = word_tokenize(mensagem.lower())
+    tokens = [stemmer.stem(word) for word in tokens if word not in stopwords]
