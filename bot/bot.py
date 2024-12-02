09e13a1088154b66260bb15527b354c28431f8b6
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-12-02 11:44:43 -0300

    removendo interactions.py, mudando a função 'identificarIntencao'

--- bot/bot.py
+++ bot/bot.py
@@ -17,24 +17,26 @@ for arquivo in arquivosCSV:
     caminho = os.path.join(dados_pasta,arquivo)
     df = pd.concat([df, pd.read_csv(caminho, encoding="latin-1",delimiter=';',on_bad_lines='skip')], ignore_index=True)
 
-# Reposta
-Bases_Conversa = {
-    "inicio":["Oi!","Olá!"],
-    "fim":["Tchau!"]
-}
+def interacoes():
+    print()
 
 # Função para identificar as palavras!
 def identificarIntencao(tokens):
-    intencao = {
-        "saudação":["eae","opa","oi","olá"],
-        "despedida":["tchau","flw"]
+    intencoes = {
+        "saudação": ["eae", "opa", "oi", "olá"],
+        "despedida": ["tchau", "flw"]
     }
-    for intencao, palavra_chave in intencao.items():
-        if any(palavra in tokens for palavra in palavra_chave):
-            return Bases_Conversa.get(intencao, "salve")
-        if palavra_chave == "tchau" or "flw":
-            return Bases_Conversa.get(intencao, "Foi um prazer conversar com você :)")
 
+    Bases_Conversa_bot = {
+        "saudação": ["Salve","Olá!","Eae","Opa"],
+        "despedida": ["Foi um prazer conversar com você :)","Tchau!"]
+    }
+
+    for chave, palavras_chave in intencoes.items():
+        if any(palavra in tokens for palavra in palavras_chave):
+            return Bases_Conversa_bot.get(chave)
+    return "Não Entendi"
+    
 # Função para tokenização e para impressão da resposta do bot
 def Tokenizacao_mensagem(mensagem):
     tokens = word_tokenize(mensagem.lower())
