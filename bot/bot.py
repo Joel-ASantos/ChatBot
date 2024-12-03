6a8e5eaaeff356760b645d53154767dc6bf9d2af
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-12-03 17:21:16 -0300

    adicionado uma função de busca para informações, remoção da leitura inicial e incorparada na função de busca

--- bot/bot.py
+++ bot/bot.py
@@ -9,18 +9,38 @@ dados_pasta = 'dados'
 arquivosCSV = [arquivo for arquivo in os.listdir(dados_pasta) if arquivo.endswith('.csv')]
 stopwords = set(stopwords.words("portuguese"))
 
-# Leitura de Dados
-df = pd.DataFrame()
-for arquivo in arquivosCSV:
-    caminho = os.path.join(dados_pasta, arquivo)
-    df = pd.concat([df, pd.read_csv(caminho, encoding="latin-1", delimiter=';', on_bad_lines='skip')], ignore_index=True)
+# Função para buscar informação que estão no dados
+def buscarInformacoes(mensagem):
+    resultado = []
+    mensagem_lower = mensagem.lower()
+     
+    for arquivo in arquivosCSV:
+        caminho = os.path.join(dados_pasta, arquivo)
+        try:
+            df = pd.read_csv(caminho, encoding="latin-1", delimiter=';', on_bad_lines='skip')
+        except Exception as e:
+            print(f"Erro ao ler o arquivo {arquivo}: {e}")
+            continue
+
+        for _, row in df.iterrows():
+            for coluna, valor in row.items():
+                if not pd.isna(valor) and mensagem_lower in str(valor).lower():
+                    resultado.append(row.to_dict())
+                    break
+    
+    if resultado:
+        resposta = "\n".join([str(res) for res in resultado[:5]])
+        return f"Encontrei essas informações:\n{resposta}"
+    else:
+        return "Não encontrei nenhuma informação relacionada"
 
 # Função para identificar as intenções
 def identificarIntencao(tokens):
     intencoes = {
         "saudação": ["eae", "opa", "oi", "olá"],
         "despedida": ["tchau", "flw", "até mais", "até logo"],
-        "elogio": ["parabéns", "muito bom", "ótimo"]
+        "elogio": ["parabéns", "muito bom", "ótimo"],
+        "buscar":["info","buscar"]
     }
 
     respostas = {
@@ -32,6 +52,8 @@ def identificarIntencao(tokens):
     tokens = [token.lower().strip(string.punctuation) for token in tokens]
     for chave, palavras_chave in intencoes.items():
         if any(palavra in " ".join(tokens) for palavra in palavras_chave):
+            if chave == "buscar":
+                return buscarInformacoes(" ".join(tokens))
             return respostas.get(chave)
     return "Não Entendi"
 
