f0648068be640bb70c04e3dff29f2f74ee7e136c
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-12-03 12:37:54 -0300

    corrigindo algumas funcionalidades e adicionando interações

--- bot/bot.py
+++ bot/bot.py
@@ -1,49 +1,47 @@
-# imports
 import os
+import string
 import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 
 # Configurações Iniciais
 dados_pasta = 'dados'
 arquivosCSV = [arquivo for arquivo in os.listdir(dados_pasta) if arquivo.endswith('.csv')]
-stopwords = set(stopwords.words("Portuguese"))
-stemmer = PorterStemmer()    
+stopwords = set(stopwords.words("portuguese"))
 
 # Leitura de Dados
 df = pd.DataFrame()
 for arquivo in arquivosCSV:
-    caminho = os.path.join(dados_pasta,arquivo)
-    df = pd.concat([df, pd.read_csv(caminho, encoding="latin-1",delimiter=';',on_bad_lines='skip')], ignore_index=True)
+    caminho = os.path.join(dados_pasta, arquivo)
+    df = pd.concat([df, pd.read_csv(caminho, encoding="latin-1", delimiter=';', on_bad_lines='skip')], ignore_index=True)
 
-def interacoes():
-    print()
-
-# Função para identificar as palavras!
+# Função para identificar as intenções
 def identificarIntencao(tokens):
     intencoes = {
         "saudação": ["eae", "opa", "oi", "olá"],
-        "despedida": ["tchau", "flw"]
+        "despedida": ["tchau", "flw", "até mais", "até logo"],
+        "elogio": ["parabéns", "muito bom", "ótimo"]
     }
 
-    Bases_Conversa_bot = {
-        "saudação": ["Salve","Olá!","Eae","Opa"],
-        "despedida": ["Foi um prazer conversar com você :)","Tchau!"]
+    respostas = {
+        "saudação": "Olá, como posso ajudar?",
+        "despedida": "Foi um prazer conversar com você.",
+        "elogio": "Muito obrigado! :)"
     }
 
+    tokens = [token.lower().strip(string.punctuation) for token in tokens]
     for chave, palavras_chave in intencoes.items():
-        if any(palavra in tokens for palavra in palavras_chave):
-            return Bases_Conversa_bot.get(chave)
+        if any(palavra in " ".join(tokens) for palavra in palavras_chave):
+            return respostas.get(chave)
     return "Não Entendi"
-    
-# Função para tokenização e para impressão da resposta do bot
+
+# Função para tokenização e identificação de intenção
 def Tokenizacao_mensagem(mensagem):
     tokens = word_tokenize(mensagem.lower())
-    tokens = [stemmer.stem(word) for word in tokens if word not in stopwords]
     resposta = identificarIntencao(tokens)
-    return print(resposta)
+    return print(f"Chatbot: {resposta}")
 
+# Loop de interação com o chatbot
 while True:
     mensagem = input("Input: ")
     Tokenizacao_mensagem(mensagem)
